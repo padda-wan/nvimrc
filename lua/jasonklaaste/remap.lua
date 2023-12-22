668aa3d438198bc652406c3d9e6b49c992758660
jklaaste <jklaaste@gmail.com>
2023-12-22 21:56:17 +0200

    Final update before trying lazyvim

--- lua/jasonklaaste/remap.lua
+++ lua/jasonklaaste/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv" , vim.cmd.Ex) 
+vim.keymap.set("n", "<leader>pv" , vim.cmd.Ex)
+
+vim.keymap.set("n", "<Up>", "<Nop>")
+vim.keymap.set("n", "<Down>", "<Nop>")
+vim.keymap.set("n", "<Left>", "<Nop>")
+vim.keymap.set("n", "<Right>", "<Nop>")
